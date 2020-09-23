e0bacab3a45b6bbd4d13afbf4115f5e7f005c594
Artjoms Travkovs <atravkovs@gmail.com>
2020-09-23 14:05:08 +0300

    Fix adjust sr numbers

--- js/components/Teams/Balance.vue
+++ js/components/Teams/Balance.vue
@@ -48,21 +48,21 @@
       </div>
       <div class="d-flex justify-content-between">
         <div class="input-group mr-2">
-          <input type="number" min="0" class="form-control" v-model="adjustSr.tank" />
+          <input type="number" min="0" class="form-control" v-model.number="adjustSr.tank" />
           <span class="input-group-text">
             <role-icon rtype="tank" />
           </span>
           <span class="input-group-text">%</span>
         </div>
         <div class="input-group mr-2">
-          <input type="number" min="0" class="form-control" v-model="adjustSr.dps" />
+          <input type="number" min="0" class="form-control" v-model.number="adjustSr.dps" />
           <span class="input-group-text">
             <role-icon rtype="dps" />
           </span>
           <span class="input-group-text">%</span>
         </div>
         <div class="input-group">
-          <input type="number" min="0" class="form-control" v-model="adjustSr.support" />
+          <input type="number" min="0" class="form-control" v-model.number="adjustSr.support" />
           <span class="input-group-text">
             <role-icon rtype="support" />
           </span>
