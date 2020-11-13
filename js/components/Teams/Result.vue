d384f587f86e398c82502aff7c6f09c16a93b128
Artjoms Travkovs <atravkovs@gmail.com>
2020-11-13 13:25:32 +0200

    Add dupsecs

--- js/components/Teams/Result.vue
+++ js/components/Teams/Result.vue
@@ -5,6 +5,7 @@
       <div>Unbalanced: {{ balance.leftovers.length }}</div>
       <div>Offroles: {{ offRolesPercentage }}%</div>
       <div>Anchors: {{ balance.anchors }}</div>
+      <div>Dupsecs: {{ dupsecCount }}</div>
     </div>
   </div>
 </template>
@@ -63,7 +64,43 @@ export default defineComponent({
       return Math.floor((offCount / totalCount) * 1000) / 10;
     });
 
-    return { addBalance, offRolesPercentage };
+    const dupsecCount = computed(() => {
+      if (!props.balance) return 0;
+
+      const c = props.balance.teams.reduce((acc, team: Team) => {
+        let accCount = acc;
+        const dps = team.members.filter((member: TeamMembers) => member.role === 'dps');
+        const tank = team.members.filter((member: TeamMembers) => member.role === 'tank');
+        const support = team.members.filter((member: TeamMembers) => member.role === 'support');
+
+        if (
+          dps.every((member: TeamMembers) => member.primary) ||
+          dps.every((member: TeamMembers) => member.secondary)
+        ) {
+          accCount += 1;
+        }
+
+        if (
+          support.every((member: TeamMembers) => member.primary) ||
+          support.every((member: TeamMembers) => member.secondary)
+        ) {
+          accCount += 1;
+        }
+
+        if (
+          tank.every((member: TeamMembers) => member.primary) ||
+          tank.every((member: TeamMembers) => member.secondary)
+        ) {
+          accCount += 1;
+        }
+
+        return accCount;
+      }, 0);
+
+      return c;
+    });
+
+    return { addBalance, offRolesPercentage, dupsecCount };
   },
 });
 </script>
