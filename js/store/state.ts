e3708723d0f86f29508b4e73b707ba5a592359c4
Artjoms Travkovs <atravkovs@gmail.com>
2021-02-01 14:28:23 +0200

    Add migration from previous version

--- js/store/state.ts
+++ js/store/state.ts
@@ -14,6 +14,28 @@ const players: Players = storage.players || {};
 const reservedPlayers: ReservedPlayers = storage.reservedPlayers || [];
 const balancerResults: Results = [];
 
+// Migration
+if (storage.balancerOptions && Array.isArray(storage.balancerOptions.adjustSr.tank)) {
+  storage.balancerOptions.adjustSr = {
+    isEnabled: false,
+    tank: {
+      any: defaultPoints(),
+      primary: defaultPoints(),
+      secondary: defaultPoints(),
+    },
+    support: {
+      any: defaultPoints(),
+      primary: defaultPoints(),
+      secondary: defaultPoints(),
+    },
+    dps: {
+      any: defaultPoints(),
+      primary: defaultPoints(),
+      secondary: defaultPoints(),
+    },
+  };
+}
+
 const balancerOptions: BalancerOptions = storage.balancerOptions || {
   range: 30,
   triesCount: 25,
