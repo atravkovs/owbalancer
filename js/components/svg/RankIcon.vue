910828812a011b4cb248331f3e5dcb36122f4dde
Artjoms Travkovs <atravkovs@gmail.com>
2021-04-23 00:52:47 +0300

    Fix -1 div

--- js/components/svg/RankIcon.vue
+++ js/components/svg/RankIcon.vue
@@ -1,22 +1,23 @@
 <template>
-  <img class="s-2em" v-if="rank <= 199" src="@/assets/divisions/div4.png" />
-  <img class="s-2em" v-else-if="rank <= 299" src="@/assets/divisions/div3.png" />
-  <img class="s-2em" v-else-if="rank <= 399" src="@/assets/divisions/div2.png" />
-  <img class="s-2em" v-else-if="rank <= 499" src="@/assets/divisions/div1.png" />
-  <img class="s-2em" v-else-if="rank <= 599" src="@/assets/sdivisions/div15.png" />
-  <img class="s-2em" v-else-if="rank <= 699" src="@/assets/sdivisions/div14.png" />
-  <img class="s-2em" v-else-if="rank <= 799" src="@/assets/sdivisions/div13.png" />
-  <img class="s-2em" v-else-if="rank <= 899" src="@/assets/sdivisions/div12.png" />
-  <img class="s-2em" v-else-if="rank <= 999" src="@/assets/sdivisions/div11.png" />
-  <img class="s-2em" v-else-if="rank <= 1099" src="@/assets/sdivisions/div10.png" />
-  <img class="s-2em" v-else-if="rank <= 1199" src="@/assets/sdivisions/div9.png" />
-  <img class="s-2em" v-else-if="rank <= 1299" src="@/assets/sdivisions/div8.png" />
-  <img class="s-2em" v-else-if="rank <= 1399" src="@/assets/sdivisions/div7.png" />
-  <img class="s-2em" v-else-if="rank <= 1499" src="@/assets/sdivisions/div6.png" />
-  <img class="s-2em" v-else-if="rank <= 1599" src="@/assets/sdivisions/div5.png" />
-  <img class="s-2em" v-else-if="rank <= 1699" src="@/assets/sdivisions/div4.png" />
-  <img class="s-2em" v-else-if="rank <= 1799" src="@/assets/sdivisions/div3.png" />
-  <img class="s-2em" v-else-if="rank <= 1899" src="@/assets/sdivisions/div2.png" />
+  <img class="s-2em" v-if="rank <= 199" src="@/assets/divisions/div5.png" />
+  <img class="s-2em" v-else-if="rank <= 299" src="@/assets/divisions/div4.png" />
+  <img class="s-2em" v-else-if="rank <= 399" src="@/assets/divisions/div3.png" />
+  <img class="s-2em" v-else-if="rank <= 499" src="@/assets/divisions/div2.png" />
+  <img class="s-2em" v-else-if="rank <= 599" src="@/assets/divisions/div1.png" />
+  <img class="s-2em" v-else-if="rank <= 699" src="@/assets/sdivisions/div15.png" />
+  <img class="s-2em" v-else-if="rank <= 799" src="@/assets/sdivisions/div14.png" />
+  <img class="s-2em" v-else-if="rank <= 899" src="@/assets/sdivisions/div13.png" />
+  <img class="s-2em" v-else-if="rank <= 999" src="@/assets/sdivisions/div12.png" />
+  <img class="s-2em" v-else-if="rank <= 1099" src="@/assets/sdivisions/div11.png" />
+  <img class="s-2em" v-else-if="rank <= 1199" src="@/assets/sdivisions/div10.png" />
+  <img class="s-2em" v-else-if="rank <= 1299" src="@/assets/sdivisions/div9.png" />
+  <img class="s-2em" v-else-if="rank <= 1399" src="@/assets/sdivisions/div8.png" />
+  <img class="s-2em" v-else-if="rank <= 1499" src="@/assets/sdivisions/div7.png" />
+  <img class="s-2em" v-else-if="rank <= 1599" src="@/assets/sdivisions/div6.png" />
+  <img class="s-2em" v-else-if="rank <= 1699" src="@/assets/sdivisions/div5.png" />
+  <img class="s-2em" v-else-if="rank <= 1799" src="@/assets/sdivisions/div4.png" />
+  <img class="s-2em" v-else-if="rank <= 1899" src="@/assets/sdivisions/div3.png" />
+  <img class="s-2em" v-else-if="rank <= 1999" src="@/assets/sdivisions/div2.png" />
   <img class="s-2em" v-else-if="rank <= 2100" src="@/assets/sdivisions/div1.png" />
   <span v-else></span>
 </template>
